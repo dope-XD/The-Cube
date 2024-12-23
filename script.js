83a620ce5e9818dc9f86c1ee1334d8681747d173
dope-XD <dopeeditz365@gmail.com>
2024-12-23 17:11:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
